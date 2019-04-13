93321cee2c6ee0b2d0330128fe6df1032fe85c6e
Horváth János <h867265@stud.u-szeged.hu>
2019-04-13 09:53:52 +0200

    number buttons

--- src/GUI.java
+++ src/GUI.java
@@ -9,17 +9,28 @@ public class GUI {
 	
 	public GUI(){
 		window = new JFrame("Hello Swing");
-		JButton b1 = new JButton("Button1");
-		JButton b2 = new JButton("Button2");
-		
+		JButton b = new JButton("Button1");
 		window.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.insets = new Insets(20, 20, 20, 20);
-		window.add(b1,c);
-		c.gridx = 1;
-		window.add(b2,c);
+		c.insets = new Insets(5, 5, 5, 5);
+		
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				b = new JButton(new Integer(j*3+i+1).toString());
+				c.gridx=i;
+				c.gridy=3-j;
+				window.add(b, c);
+			}
+		}
+		for(int i=0; i<2; i++){
+			b = new JButton("0.".substring(i, i+1));
+			c.gridy=4;
+			c.gridx=i;
+			window.add(b,c);
+		}
+		
+		
+		
 		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(640, 480);
