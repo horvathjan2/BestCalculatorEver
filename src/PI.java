0c757626c64285189117c8c44979bc6b320e4f72
Horváth János <h867265@stud.u-szeged.hu>
2019-05-11 11:17:35 +0200

    changed pi to Math.PI

--- src/PI.java
+++ src/PI.java
@@ -4,9 +4,10 @@
  * PI is a default constant that is used by the calculator.
  * Extends Operation_0.
  */
+
 public class PI extends Operation_0 {
     public Double calc() {
-        return 3.14159265359;
+        return Math.PI;
     }
     public String getSymbol() {
         return "π";
